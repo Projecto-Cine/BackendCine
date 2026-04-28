538950c5b1f39dc2f9b575786345489bf83894df
ana-morandeira <ana.agrodorolo@gmail.com>
2026-04-28 18:19:44 +0200

    chore: add model structure

---
+++ src/main/java/com/cine/demo/model/Purchase.java
@@ -0,0 +1,17 @@
+package com.cine.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/cine/demo/model/Seat.java
@@ -0,0 +1,17 @@
+package com.cine.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
