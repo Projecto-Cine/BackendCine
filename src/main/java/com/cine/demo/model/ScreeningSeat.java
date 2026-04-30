d753d0d09d1c081c7cd69815b145777a10521dc9
ana-morandeira <ana.agrodorolo@gmail.com>
2026-04-30 17:42:49 +0200

    feat(theater): add Theater, Seat, Movie, Screening and ScreeningSeat entities with JPA and Lombok

---
+++ src/main/java/com/cine/demo/model/ScreeningSeat.java
@@ -0,0 +1,35 @@
+package com.cine.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "screening_seats", uniqueConstraints = @UniqueConstraint(columnNames = {"screening_id", "seat_id"}))
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScreeningSeat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "screening_id")
+    @NotNull
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Screening screening;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id")
+    @NotNull
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Seat seat;
+
+    @Builder.Default
+    private boolean ocupado = false;
+}
--- src/main/java/com/cine/demo/model/Theater.java
+++ src/main/java/com/cine/demo/model/Theater.java
@@ -1,17 +1,43 @@
 package com.cine.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "theaters")
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Theater {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    private String nombre;
+
+    @Min(1)
+    private int capacidad;
+
+    @OneToMany(mappedBy = "theater", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Seat> seats = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
